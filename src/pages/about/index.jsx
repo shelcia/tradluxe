d8c9625ed4a4c07c0f8acc6570295050248c8b71
Narasimhan <edm18b040@iiitdm.ac.in>
2023-12-06 10:44:52 +0530

    revert changes

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -297,7 +297,7 @@ const AboutPage = () => {
           with both customers and partners. We look forward to serving the needs
           of consumers and contributing to the growth of our industry.
         </Typography>
-       {/* <CustomAboutDivider />
+       <CustomAboutDivider />
         <Box sx={{ pt: 4 }}>
           <CustomTitle subtitle="Find us" title="Our Retail Presence" />
         </Box>
@@ -379,7 +379,7 @@ const AboutPage = () => {
             />
           </Container>
         </Box>
-          </Box>*/}
+          </Box>
     </>
   );
 };
